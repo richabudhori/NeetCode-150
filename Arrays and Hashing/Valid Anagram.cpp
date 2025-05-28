b0f703e5292b4c80515b2cb7daf89b7b4f666503
Richa Budhori <141902724+richabudhori@users.noreply.github.com>
2025-05-28 06:13:53 +0530

    Create Valid Anagram.cpp

---
+++ Arrays and Hashing/Valid Anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        //check if the frequency of both strings are the same or not
+        if(s.length() != t.length()){
+            return false;
+        }
+        //sorting the strings by sort function
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        return s==t;
+
+    }
+};
